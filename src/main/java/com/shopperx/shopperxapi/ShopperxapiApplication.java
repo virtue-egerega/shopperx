3bf115d702b607ab0d7ee3e1d28d39d7c04c0f6c
Virtue Egerega <egeregav@gmail.com>
2025-08-03 09:30:00 +0100

    Initialization: project initialization

---
+++ src/main/java/com/shopperx/shopperxapi/ShopperxapiApplication.java
@@ -0,0 +1,13 @@
+package com.shopperx.shopperxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopperxapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopperxapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shopperx/shopperxapi/ShopperxapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopperx.shopperxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopperxapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
